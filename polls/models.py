24c0d301d134808b07819131cddeb11edc52dbfd
ecotalisman <ecotalisman@gmail.com>
2023-05-20 23:36:56 +0300

    Finished creating a poll form according to the task

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+from django.db import models
 
 
 class Question(models.Model):
@@ -26,6 +27,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='choices/', blank=True, null=True)
 
     def __str__(self):
         return self.choice_text
